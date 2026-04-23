97cc7dd0ecc27121cdbff10539e51ed3175abf4d
noya <noya.cohen@a-y.org.il>
2026-04-23 10:37:30 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+<script>
+    function showSection(sectionId) {
+    const sections. = document.querySelectorAll('.section');
+    sections.forEach(section => {
+    section.style.display = 'none'});
+    document.getElementById(sectionId).style.display = 'block';
+
+    }
+
+
+</script>
